eed8bc2b11100450f2726f29dce036e77c5da3ee
fpereira-epfl <francisco.pinto@epfl.ch>
2026-03-23 11:58:19 +0100

    🐛 Fix method calls to use instance method execute_query in query execution

--- graphdb/core/graphdb.py
+++ graphdb/core/graphdb.py
@@ -935,7 +935,7 @@ class GraphDB():
                 print(sql_query_eval)
 
             # Execute the query
-            out = db.execute_query(engine_name=engine_name, query=sql_query_eval, params=sql_params)
+            out = self.execute_query(engine_name=engine_name, query=sql_query_eval, params=sql_params)
 
             # Build up the test results dictionary
             print_colour(f'\nChanges on table {t}:', style='bold')
@@ -987,7 +987,7 @@ class GraphDB():
 
         # Execute commit
         if 'commit' in actions:
-            out = db.execute_query(engine_name='xaas_coresrv', query=sql_query_commit, params=sql_params, commit=True, return_exception=True)
+            out = self.execute_query(engine_name='xaas_coresrv', query=sql_query_commit, params=sql_params, commit=True, return_exception=True)
             if not type(out) is list:
                 error_type, error_msg, dbapi_code = out
                 if dbapi_code==1062: # Duplicate entry
